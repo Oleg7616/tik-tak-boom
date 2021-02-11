787540e95bafc35e722ef94a5df3666415a1b599
Oleg Leontjev <oleg.leontjev@mail.ru>
2021-02-11 06:16:45 +0200

    init

---
+++ js/tikTakBoomTasks.js
@@ -0,0 +1,85 @@
+const tasks = `
+    [
+        {
+            "question": "2 * 2 = ",
+            "answer1": { "result": true, "value": "4" },
+            "answer2": { "result": false, "value": "3" }
+        },
+        {
+            "question": "Как зовут крокодила, лучшего друга Чебурашки?",
+            "answer1": { "result": false, "value": "Данди" },
+            "answer2": { "result": true, "value": "Гена" }
+        },
+        {
+            "question": "5 + 5",
+            "answer1": { "result": false, "value": "1" },
+            "answer2": { "result": true, "value": "10" }
+        },
+        {
+            "question": "Столица Италии?",
+            "answer1": { "result": false, "value": "Ватикан" },
+            "answer2": { "result": true, "value": "Рим" }
+        }
+    ]
+`;
+
+const tasks1 = `
+
+[
+    {
+        "question": "Шахматы - вид спорта?",
+            "answer1": { "result": true, "value": "Да" },
+            "answer2": { "result": false, "value": "Нет" }
+    },
+    {
+        "question": "Инструмент для игры в гольф.",
+            "answer1": { "result": true, "value": "Клюшка" },
+            "answer2": { "result": false, "value": "Бита" }
+    },
+    {
+        "question": "Родина хоккея с шайбой.",
+        "answer1": { "result": true, "value": "Канада" },
+        "answer2": { "result": false, "value": "Швеция" }
+    },
+    {
+        "question": "Марафонский забег - длина дистанции?",
+        "answer1": { "result": true, "value": "42195 м" },
+        "answer2": { "result": false, "value": "50500 м" }
+    },
+    {
+        "question": "Удар в боксе.",
+        "answer1": { "result": true, "value": "Апперкот" },
+        "answer2": { "result": false, "value": "Антрекот" }
+    },
+    {
+        "question": "Пеле.",
+        "answer1": { "result": true, "value": "Знаменитый футболист" },
+        "answer2": { "result": false, "value": "Знаменитый пловец" }
+    },
+    {
+        "question": "Тайм в футболе.",
+        "answer1": { "result": false, "value": "90 мин" },
+        "answer2": { "result": true, "value": "45 мин" }
+    },
+    {
+        "question": "Штрафной удар в футболе.",
+        "answer1": { "result": false, "value": "Буллит" },
+        "answer2": { "result": true, "value": "Пенальти" }
+    },
+    {
+        "question": "В какой спортивной дисциплине используется шест?",
+        "answer1": { "result": false, "value": "Прыжки в воду" },
+        "answer2": { "result": true, "value": "Прыжки в высоту" }
+    },
+    
+    {
+        "question": "Высшая оценка в фигургом катании.",
+        "answer1": { "result": false, "value": "5+" },
+        "answer2": { "result": true, "value": "6.0" }
+    },
+    {
+        "question": "Высшая оценка в фигургом катании.",
+        "answer1": { "result": false, "value": "5+" },
+        "answer2": { "result": true, "value": "6.0" }
+    },
+]`
